8dafe1ffdf2ba8dca24047ad1b95884f713053ef
SaharAvr <64190984+SaharAvr@users.noreply.github.com>
2021-10-31 22:36:27 +0200

    [FEAT] component tree - add support for inner components

--- src/sassKit.js
+++ src/sassKit.js
@@ -4,7 +4,7 @@ const md5 = require('md5');
 const React = require('react');
 const sassKitParser = require('./sassKitParser');
 const sassKitElement = require('./sassKitElement');
-const { DEFALUT_CLASSNAME_PREFIX, DOMAttributes } = require('./sassKitConsts');
+const { DEFALUT_CLASSNAME_PREFIX, DOMAttributes, IntrinsicElements } = require('./sassKitConsts');
 
 const makeStyles = superClasses => {
 
@@ -33,33 +33,84 @@ const makeStyles = superClasses => {
 
 const extend = Component => (...args) => {
 
-  const state = {
+  const settings = {
     componentName: (
       _.get(Component, 'Naked.render.name') ||
       _.get(Component, 'name', DEFALUT_CLASSNAME_PREFIX)
     ),
     className: '',
     didUpdateSuperClasses: false,
+    finalComponent: null,
+    isInitialised: false,
+  };
+
+  const init = ({ children, ...props }) => {
+
+    if (settings.isInitialised) {
+      return;
+    }
+
+    settings.finalComponent = (() => {
+
+      if (!_.isFunction(Component)) {
+        return Component;
+      }
+  
+      let InnerComponent = Component({ children, ...props });
+
+      while (_.isFunction(InnerComponent.type)) {
+        InnerComponent = InnerComponent.type({ ...InnerComponent.props });
+      }
+  
+      return InnerComponent;
+  
+    })();
+
+    settings.finalComponentType = _.get(settings.finalComponent, 'type', '').toString();
+
+    settings.className = (() => {
+
+      const cssStyle = `${sassKitParser.parseWithProps(props, ...args)}\n${_.get(sassKitElement.superClasses, props.className, '')}`;
+      const className = `${settings.componentName}-${md5(cssStyle).substring(0, 5)}`;
+      settings.didUpdateSuperClasses = true;
+
+      sassKitElement.updateWith({ [className]: cssStyle });
+
+      return className;
+
+    })();
+
+    settings.isInitialised = true;
+
   };
 
   return ({ children, ...props }) => {
     
-    if (!state.didUpdateSuperClasses) {
-
-      const cssStyle = `${sassKitParser.parseWithProps(props, ...args)}\n${_.get(sassKitElement.superClasses, props.className, '')}`;
-      state.className = `${state.componentName}-${md5(cssStyle).substring(0, 5)}`;
-      state.didUpdateSuperClasses = true;
-
-      sassKitElement.updateWith({ [state.className]: cssStyle });
-
+    if (!settings.isInitialised) {
+      init({ children, ...props });
     }
 
-    const shouldRenderComponent = _.has(Component, 'render');
-    const finalComponent = (shouldRenderComponent ? Component : _.camelCase(_.first(React.Children.toArray(children))));
+    const isDOMTypeElement = _.includes(IntrinsicElements, settings.finalComponentType);
+    if (isDOMTypeElement) {
+      return React.createElement(
+        settings.finalComponentType,
+        { ...settings.finalComponent.props, className: settings.className },
+        settings.finalComponent.props.children,
+      );
+    }
+
+    const isReactFragment = (settings.finalComponentType === 'Symbol(react.fragment)');
+    if (isReactFragment) {
+      return React.createElement(
+        'div',
+        { ...settings.finalComponent.props, className: settings.className },
+        settings.finalComponent.props.children,
+      );
+    }
 
     return React.createElement(
-      finalComponent,
-      { ..._.pick(props, DOMAttributes), className: state.className },
+      Component,
+      { ..._.pick(props, DOMAttributes), className: settings.className },
       children,
     );
 
